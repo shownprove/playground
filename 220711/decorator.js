9a1a4436aa79d09b57d8f317db8592b9361ca2e5
oddcode <oddcode@naver.com>
2022-07-11 22:54:19 +0900

    more decorator

--- 220711/decorator.js
+++ 220711/decorator.js
@@ -1,1 +1,17 @@
-function 
+function simpleDecorator(x) {
+  console.log(
+    `simpleDecorator의 실행 결과 Class ${x}를 인자로 받아 가공해줍니다.`
+  );
+  return newClass extends x {
+
+  }
+}
+
+@simpleDecorator()
+class Decorated {
+  constructor(x, y) {
+    this.plus = x + y;
+    this.minus = x - y;
+  }
+}
+
