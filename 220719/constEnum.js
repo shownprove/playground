c1b534e9d66c03e674558562d0668a6388350c10
oddcode <oddcode@naver.com>
2022-07-19 01:43:33 +0900

    const Enum 관련

---
+++ 220719/constEnum.js
@@ -0,0 +1,15 @@
+const Direction = {
+  Up: 0,
+  Down: 1,
+  Left: 2,
+  Right: 3,
+};
+
+let directions = [
+  Direction.Up,
+  Direction.Down,
+  Direction.Left,
+  Direction.Right,
+];
+
+console.log(directions);
